cceacebb2eeca7c2a8c154754531463b9946e0d2
leem5514 <ikaros0569@gmail.com>
2024-09-05 19:35:57 +0900

    fix

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -238,15 +238,7 @@ export default {
                 this.client.deactivate();
             }
         },
-        ensureRoomSubscription(roomId) {
-            axios.post(`${process.env.VUE_APP_API_BASIC_URL}/chat/ensure-subscription`, { roomId })
-                .then(() => {
-                    console.log('Room subscription ensured');
-                })
-                .catch(error => {
-                    console.error('Failed to ensure room subscription:', error);
-                });
-        },
+
 
 
         joinRoom() {
